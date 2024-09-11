0cbea5c19f965bc4823e2d6e726908bfe0850cfd
hp <hp@DESKTOP-R2I829L>
2024-09-11 22:58:15 +0530

    Add new code

---
+++ newJavaProject/src/newJavaPackage/BreakStatement3.java
@@ -0,0 +1,22 @@
+package newJavaPackage;
+
+public class BreakStatement3 {
+
+	public static void main(String[] args) {
+       
+		int i = 1 ;
+		
+		while(i<=10) {
+			
+			if(i==6) {
+				break;
+			}
+			System.out.println(i);
+			i++ ;
+		}
+		System.out.println("control came out from loop");
+		System.out.println("Add new code");
+	}
+	
+
+}
